033f4d717e922390ffcf7218b8e8677dc95fd8a0
João Tiago <joao.leal.tintas@tecnico.ulisboa.pt>
2024-03-23 20:13:43 +0000

    initial commit + navbar

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <PageHeader/>
+</template>
+
+<script>
+import PageHeader from './components/PageHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PageHeader
+  }
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        cryptoTrackYellow: '#FBA834',
+        cryptoTrackBlue1: '#333A73',
+        cryptoTrackBlue2: '#387ADF',
+        cryptoTrackBlue3: '#50C4ED',
+      },
+    },
+  },
+  plugins: [],
+}
