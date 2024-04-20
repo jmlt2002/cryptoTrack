6d48114831dd1a7bfb915cf2d60ee94f6daa9a53
João Tiago <joao.leal.tintas@tecnico.ulisboa.pt>
2024-04-20 21:07:29 +0100

    feat: add pagination to Coin List Table

--- src/components/CoinList.vue
+++ src/components/CoinList.vue
@@ -3,6 +3,10 @@
     <p class="text-3xl">List of available coins</p>
   </div>
   <div class="flex justify-center">
+    <button id="prev" @click="prev" :disabled="currentPage === 0" class="btn rounded-badge mb-5 ml-5 mr-5"><span class="text-xl mb-1" aria-hidden="true">&larr;</span>Back</button>
+    <button id="next" @click="next" :disabled="currentPage === maxPage" class="btn rounded-badge mb-5 ml-5 mr-5">Next<span class="text-xl mb-1" aria-hidden="true">&rarr;</span></button>
+  </div>
+  <div class="flex justify-center">
     <div class="w-full lg:w-1/2">
       <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table class="w-full text-sm text-left rtl:text-right table-fixed">
@@ -32,6 +36,10 @@
       </div>
     </div>
   </div>
+  <div class="flex justify-center">
+    <button id="prev" @click="prev" :disabled="currentPage === 0" class="btn rounded-badge m-5"><span class="text-xl mb-1" aria-hidden="true">&larr;</span>Back</button>
+    <button id="next" @click="next" :disabled="currentPage === maxPage" class="btn rounded-badge m-5">Next<span class="text-xl mb-1" aria-hidden="true">&rarr;</span></button>
+  </div>
 </template>
 
 <script>
@@ -40,23 +48,41 @@ import { createClient } from '@supabase/supabase-js'
 export default {
   data() {
     return {
-      coins: [] // Initialize coins array to hold fetched data
+      coins: [], // Initialize coins array to hold fetched data
+      currentPage: 0,
+      maxPage: 2,
     }
   },
   async mounted() {
-    const supabase = createClient('https://sfqpiwoexeusferirizl.supabase.co', process.env.VUE_APP_SUPABASE_API_KEY)
+    this.fetchCoinsData()
+  },
+  methods: {
+    async fetchCoinsData() {
+      const supabase = createClient('https://sfqpiwoexeusferirizl.supabase.co', process.env.VUE_APP_SUPABASE_API_KEY)
 
-    try {
-      const { data: coins, error } = await supabase.from('coinsList').select()
+      try {
+        // Fetch data from the coinsList table where id >= currentPage * 1000
+        const { data: coins, error } = await supabase.from('coinsList').select().range(this.currentPage * 1000, (this.currentPage + 1) * 1000)
 
-      if (error) {
+        if (error) {
+          console.error(error)
+        } else {
+          this.coins = coins // Update coins data in the component
+        }
+      } catch (error) {
         console.error(error)
-      } else {
-        this.coins = coins // Update coins data in the component
       }
-    } catch (error) {
-      console.error(error)
-    }
-  }
+    },
+    next() {
+      if (this.currentPage === this.maxPage) return
+      this.currentPage += 1
+      this.fetchCoinsData()
+    },
+    prev() {
+      if (this.currentPage === 0) return
+      this.currentPage -= 1
+      this.fetchCoinsData()
+    },
+  },
 }
 </script>
